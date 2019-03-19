001439f6be133dfd17843121b795ebf594c07142
ggboy <switch2018@outlook.com>
2019-03-19 20:06:36 +0800

    init

--- js/md.js
+++ js/md.js
@@ -1,16 +1,18 @@
-var markedjs = document.createElement("script");
+let markedjs = document.createElement("script");
 markedjs.setAttribute("type", "text/javascript");
-markedjs.setAttribute("src", "/static/js/marked.min.js");
-var highlight = document.createElement("script");
+markedjs.setAttribute("src", "/js/marked.min.js");
+let highlight = document.createElement("script");
 highlight.setAttribute("type", "text/javascript");
-highlight.setAttribute("src", "/static/js/highlight.pack.js");
+highlight.setAttribute("src", "/js/highlight.pack.js");
 document.body.appendChild(markedjs);
 document.body.appendChild(highlight);
 
-var md = {
+let md = {
     isInit : false,
     init : function () {
         marked.setOptions({
+            gfm: true,
+            tables : true,
             langPrefix: 'hljs lang-',
             breaks: true,
             headerIds: false,
@@ -28,39 +30,39 @@ var md = {
     parseAuto : function (before, after) {
         if (after === undefined)
             after = before;
-        var text = typeof(before)=='string' ? before : before.innerHTML;
+        let text = typeof(before)=='string' ? before : before.innerHTML;
         // text = text.replace(/&(lt|gt|nbsp|amp|quot);/ig,function(all,t){return {'lt':'<','gt':'>','nbsp':' ','amp':'&','quot':'"'}[t];});
         convertCode(after, this.parse(text));
     }
 }
 
 function convertCode(element, contentStr) {
-    var content = document.createElement("div");
+    let content = document.createElement("div");
     content.innerHTML = contentStr;
     content.querySelectorAll('code.hljs').forEach(function (codeElement) {
-        var ul = document.createElement("ul");
+        let showLine = false;
+        let ul = document.createElement("ul");
         codeElement.innerHTML.split(/[\n]/g).forEach(function (line, index) {
-            var li = document.createElement("li");
+            if (index == 0 && line == ":set number") {
+                showLine = true;
+                return;
+            }
+            let li = document.createElement("li");
             li.setAttribute("onClick", 'selectLine(this)');
-            var numDiv = document.createElement("span");
-            numDiv.className = 'hljs-ln-num';
-            numDiv.setAttribute("ln-num", index + 1);
-            var codeDiv = document.createElement("span");
-            codeDiv.className = 'hljs-ln-code';
-            codeDiv.innerHTML = line;
-            li.appendChild(numDiv);
-            li.appendChild(codeDiv);
+            if (showLine)
+                setLineNumber(li, index);
+            setCode(li, line);
             ul.appendChild(li);
         });
         clean(codeElement).appendChild(ul);
     });
 
     clean(element);
-    for (var i = 0; i < content.childNodes.length; i++)
+    for (let i = 0; i < content.childNodes.length; i++)
         element.appendChild(content.childNodes[i]);
 
     element.querySelectorAll('code.hljs ul').forEach(function (ulElement) {
-        var width = ulElement.scrollWidth + "px";
+        let width = ulElement.scrollWidth + "px";
         ulElement.childNodes.forEach(function(liElement){
             liElement.style.width = width;
         });
@@ -75,7 +77,21 @@ function clean(element) {
 }
 
 function selectLine(element) {
-    for (var child = element.parentNode.firstChild; child; child = child.nextSibling)
+    for (let child = element.parentNode.firstChild; child; child = child.nextSibling)
         child.classList.remove('selected');
     element.classList.add('selected');
+}
+
+function setLineNumber(element, num) {
+    let lineNumber = document.createElement("span");
+    lineNumber.className = 'hljs-ln-num';
+    lineNumber.setAttribute("ln-num", num);
+    element.appendChild(lineNumber);
+}
+
+function setCode(element, str) {
+    let code = document.createElement("span");
+    code.className = 'hljs-ln-code';
+    code.innerHTML = str;
+    element.appendChild(code);
 }
