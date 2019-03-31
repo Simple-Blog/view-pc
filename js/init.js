319e3af7df729f565da302af1c576cdaf733bfaf
ggboy <switch2018@outlook.com>
2019-03-31 23:56:37 +0800

    优化代码 - 未完成

--- js/init.js
+++ js/init.js
@@ -1,24 +1,31 @@
-layui.define(['httputil', 'jquery', 'laytpl'], function(exports){
+layui.define(['httputil', 'jquery', 'laytpl'], function(exports) {
     let httputil = layui.httputil;
-    let jquery = layui.jquery;
     let laytpl = layui.laytpl;
 
-    exports('init', function(){
-        jquery('.templet').each(async function(){
-            jquery(this).hide();
-            let url = jquery(this).attr('url');
-            let html = jquery(this).html();
-
-            try {
-                let result = await httputil.get(url);
-                html = laytpl(html).render(result.data != null ? result.data : {});
-                jquery(this).html(html);
-                jquery(this).show();
-                return;
-            } catch (e) {
-                console.log("获取响应参数失败");
-                console.log(e);
-            }
-        });
+    exports('init', function () {
+        parseTemplet(document);
     });
+
+    async function parseTemplet(element) {
+        while (element.getElementsByTagName("templet").length > 0) {
+            await parseTemplet(element.getElementsByTagName("templet")[0]);
+        }
+
+        if (element == document)
+            return;
+
+        let url = element.getAttribute('url');
+        if (url == "")
+            return;
+
+        try {
+            let html = element.innerHTML;
+            let result = await httputil.get(url);
+            html = laytpl(html).render(result.data != null ? result.data : {});
+            element.outerHTML = html;
+        } catch (e) {
+            console.log("获取响应参数失败");
+            console.log(e);
+        }
+    }
 });
